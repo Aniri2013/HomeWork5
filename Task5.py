91c5d1ae2f01fc856ea84ca99f372e1d41d83399
Aniri2013 <aniri-13.rodina>
2019-08-28 18:21:43 +0300

    commit

--- Task5.py
+++ Task5.py
@@ -1,11 +1,10 @@
-massiv = {}
-razm = int(input("Введите количество массивов = "))
-dict = {}
-
-for i in range(razm):
-    massiv[i] = input("Элементы[" + str(i) + "] = ")
-    dict[massiv[i]] = len(massiv[i])
-
-dict = sorted(dict.items(), key=lambda x: x[1],
-for i in range(razm):
-    print(dict[i][0])
+massiv = []
+ryad = int(input('Количество рядков: '))
+stolb = int(input('Количество столбиков: '))
+for i in range(ryad):
+    massiv.append([])
+    for j in range(stolb):
+        massiv[i].append(int(input('Введи число ')))
+for i in range(ryad):
+    massiv[i].sort()
+print(massiv)
